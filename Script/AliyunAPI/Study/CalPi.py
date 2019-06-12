3dd35872a4a5983754cb4c241af64fb8d0daf971
tinywind521 <51765405@qq.com>
2019-06-12 14:06:05 +0800

    20190612-1400

--- Script/AliyunAPI/Study/CalPi.py
+++ Script/AliyunAPI/Study/CalPi.py
@@ -1,14 +1,13 @@
 import time
 import random
 
-n = 0
 i = 0
-while True:
+for n in range(1, 9999999999):
     x = random.random()
     y = random.random()
     if x**2 + y**2 <= 1:
         i = i + 1
-    n = n + 1
     pi = 4 * i / n
-    print('\ri = {}, n = {}, pi = {}'.format(i, n, pi),end='')
-    time.sleep(0.0000)
+    print(n)
+print('\rpi = {}\tn = {}'.format(pi, n),end='')
+    # time.sleep(0.0000)
